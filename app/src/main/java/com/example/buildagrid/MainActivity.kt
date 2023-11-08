99bf7299f5987f09e298d8db134de32cb96918d5
Alibek <developeruzbekiston@mail.ru>
2023-11-08 16:09:13 +0500

    learning lazy grid with local data source

--- app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -4,26 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.buildagrid.data.Datasource
 import com.example.buildagrid.model.Topic
 import com.example.buildagrid.ui.theme.BuildAGridTheme
 
@@ -33,9 +38,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             BuildAGridTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicGridList(
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
@@ -43,29 +51,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
-    Card(
-        modifier = modifier, shape = RoundedCornerShape(6.dp), colors = CardDefaults.cardColors()
+fun TopicGridList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
     ) {
-        Image(
-            painter = painterResource(id = topic.image),
-            contentDescription = stringResource(id = topic.title),
-            modifier = Modifier
-                .width(68.dp)
-                .height(68.dp)
-        )
-        Column(modifier = Modifier.padding(top = 16.dp, end = 16.dp)) {
-            Text(
-                text = stringResource(id = R.string.photography),
-                modifier = Modifier.padding(start = 16.dp)
-            )
-            Row(modifier = Modifier.padding(top = 8.dp)) {
+        items(Datasource().loadArtSpace()) {topic ->
+            TopicCard(topic = topic)
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card() {
+        Row {
+            Box {
                 Image(
-                    painter = painterResource(id = R.drawable.painting),
-                    contentDescription = "Cube",
-                    modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    painter = painterResource(id = topic.image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
                 )
-                Text(text = stringResource(id = topic.number))
+            }
+            Column(modifier = modifier.padding(top = 16.dp, end = 16.dp)) {
+                Text(
+                    text = stringResource(id = topic.title),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    )
+                    Text(
+                        text = topic.number.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
             }
         }
     }
@@ -74,11 +106,12 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun TopicCardPreview() {
-    TopicCard(
-        topic = Topic(
-            image = R.drawable.photographer,
-            title = R.string.photography,
-            number = (0..100).random()
-        )
-    )
+    val topic = Topic(R.drawable.photographer, 321, R.string.photography)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopicCard(topic = topic)
+    }
 }
--- app/src/main/java/com/example/buildagrid/data/Datasource.kt
+++ app/src/main/java/com/example/buildagrid/data/Datasource.kt
@@ -8,15 +8,15 @@ class Datasource {
     fun loadArtSpace(): List<Topic> {
         return listOf(
             Topic(R.drawable.business, 78, R.string.business),
-            Topic(R.drawable.fashion, 78, R.string.fashion),
-            Topic(R.drawable.film, 78, R.string.film),
-            Topic(R.drawable.gaming, 78, R.string.gaming),
-            Topic(R.drawable.drawing, 78, R.string.drawing),
-            Topic(R.drawable.lifestyle, 78, R.string.lifestyle),
-            Topic(R.drawable.music, 78, R.string.music),
-            Topic(R.drawable.painting, 78, R.string.painting),
-            Topic(R.drawable.photographer, 78, R.string.photography),
-            Topic(R.drawable.tech, 78, R.string.tech)
+            Topic(R.drawable.fashion, 92, R.string.fashion),
+            Topic(R.drawable.film, 165, R.string.film),
+            Topic(R.drawable.gaming, 164, R.string.gaming),
+            Topic(R.drawable.drawing, 326, R.string.drawing),
+            Topic(R.drawable.lifestyle, 305, R.string.lifestyle),
+            Topic(R.drawable.music, 212, R.string.music),
+            Topic(R.drawable.painting, 172, R.string.painting),
+            Topic(R.drawable.photographer, 321, R.string.photography),
+            Topic(R.drawable.tech, 118, R.string.tech)
         )
     }
 }
